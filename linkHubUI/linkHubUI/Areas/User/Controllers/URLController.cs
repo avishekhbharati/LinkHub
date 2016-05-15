9dc00ff1e20ce2f0f564c70b4e9cb4057183403e
avishekh <avishekh.bharati@gmail.com>
2016-05-15 08:22:14 +0545

    Created Areas and the controller and UI for URL

---
+++ linkHubUI/linkHubUI/Areas/User/Controllers/URLController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using BOL;
+
+namespace linkHubUI.Areas.User.Controllers
+{
+    public class URLController : Controller
+    {
+        // GET: User/URL
+        public ActionResult Index()
+        {
+            LinkHubDbEntities db = new LinkHubDbEntities();
+            ViewBag.CategoryId = new SelectList(db.tbl_Category, "CategoryId", "CategoryName");
+            return View();
+        }
+
+
+        [HttpPost]
+        public ActionResult Create(tbl_Url model)
+        {
+            //Addition logic here 
+            return View("Index");
+        }
+    }
+}
